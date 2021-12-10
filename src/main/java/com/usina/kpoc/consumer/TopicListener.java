89c190ff20ade8c9c7ddd052df2aed0debe0be11
Otavio Marchioli <otavio.marchioli@gmail.com>
2021-12-10 17:20:18 -0300

    primeiro commit - adicionando kafka listener

---
+++ src/main/java/com/usina/kpoc/consumer/TopicListener.java
@@ -0,0 +1,26 @@
+package com.usina.kpoc.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class TopicListener {
+
+    @Value("${topic.name.consumer")
+    private String topicName;
+
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload){
+    	log.info("Tópico: {}", topicName);
+        log.info("key: {}", payload.key());
+        log.info("Headers: {}", payload.headers());
+        log.info("Partion: {}", payload.partition());
+        log.info("Order: {}", payload.value());
+    }
+
+}
